96f88b24b67127d6a45d20bdcf81bf46886649b7
PKTseng <jk0910948659@Gmail.com>
2020-11-21 17:39:59 +0800

    v1

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view />
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.MYPATH}MYPATH/products`
+    console.log(process.env.APIPATH, process.env.MYPATH)
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    })
+  }
+}
+</script>
+
+<style lang='scss'>
+@import './assets/all';
+</style>
