9e987ae1ba276cba870e1bd1257c3a0ab6f3a9c0
PKTseng <jk0910948659@Gmail.com>
2020-11-22 21:46:45 +0800

    new ok

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -3,7 +3,8 @@
     <div class="text-right mt-3">
       <button
         class="btn btn-outline-primary"
-        id='productModel'
+        data-toggle="modal"
+        data-target="#productModel"
         @click='openModel'
       >
         建立新產品
@@ -45,51 +46,6 @@
           </td>
         </tr>
       </tbody>
-      <!-- Modal -->
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div
-          class="modal-dialog"
-          role="document"
-        >
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5
-                class="modal-title"
-                id="exampleModalLabel"
-              >Modal title</h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-dismiss="modal"
-              >Close</button>
-              <button
-                type="button"
-                class="btn btn-primary"
-              >Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
     </table>
   </div>
 
